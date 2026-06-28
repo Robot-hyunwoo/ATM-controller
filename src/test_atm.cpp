79b2746195300e48a12563e9b4871ae792d72bd2
Robot-hyunwoo <mine5264@naver.com>
2026-06-28 18:34:49 +0900

    [feat] Add 5 test case

--- src/test_atm.cpp
+++ src/test_atm.cpp
@@ -79,7 +79,7 @@ static VirtualBankSystem initializeBankSystem() {
   bank_service.setBalance("account_2", 500);
 
   bank_service.addCard("01020304", "1234", {"account_3"});
-  bank_service.setBalance("account_3", 120);
+  bank_service.setBalance("account_3", 800);
   return bank_service;
 }
 
@@ -96,15 +96,76 @@ void testWithDrawInsufficientFunds() {
   VirtualCashSystem cash_bin = initializeCashSystem();
   ATMController atm(bank_service, cash_bin);
 
-  std::cout << "[1] Test insufficient funds case." << std::endl;
-  atm.insertCard("12345678");
-  atm.enterPin("0000");
-  atm.selectAccount("account_1");
-  atm.withdraw(200);
+  std::cout << "[1] Test account insufficient funds case." << std::endl;
+
+  try {
+    atm.insertCard("12345678");
+    atm.enterPin("0000");
+    atm.selectAccount("account_1");
+    atm.withdraw(200);
+  } catch (std::runtime_error& e) { std::cout << e.what() << std::endl; } // pass
+}
+
+void testWrongPinNumber() {
+  VirtualBankSystem bank_service = initializeBankSystem();
+  VirtualCashSystem cash_bin = initializeCashSystem();
+  ATMController atm(bank_service, cash_bin);
+
+  std::cout << "[2] Test wrong pin number case." << std::endl;
+  try {
+    atm.insertCard("01020304");
+    atm.enterPin("0000");
+  } catch (std::runtime_error& e) { std::cout << e.what() << std::endl; } // pass
+}
+
+void testThereIsNoAccount() {
+  VirtualBankSystem bank_service = initializeBankSystem();
+  VirtualCashSystem cash_bin = initializeCashSystem();
+  ATMController atm(bank_service, cash_bin);
+
+  std::cout << "[3] Test wrong account select case." << std::endl;
+  try {
+    atm.insertCard("01020304");
+    atm.enterPin("1234");
+    atm.selectAccount("account_1");
+  } catch (std::runtime_error& e) { std::cout << e.what() << std::endl; } // pass
+}
+
+void testMinusDollar() {
+  VirtualBankSystem bank_service = initializeBankSystem();
+  VirtualCashSystem cash_bin = initializeCashSystem();
+  ATMController atm(bank_service, cash_bin);
+
+  std::cout << "[4] Test minus dollar case." << std::endl;
+  try {
+    atm.insertCard("12345678");
+    atm.enterPin("0000");
+    atm.selectAccount("account_1");
+    atm.deposit(-10);
+  } catch (std::runtime_error& e) { std::cout << e.what() << std::endl; } // pass
+}
+
+void testCashInsufficientFunds() {
+  VirtualBankSystem bank_service = initializeBankSystem();
+  VirtualCashSystem cash_bin = initializeCashSystem();
+  ATMController atm(bank_service, cash_bin);
+
+  std::cout << "[5] Test cash insufficient funds case." << std::endl;
+  try {
+    atm.insertCard("01020304");
+    atm.enterPin("1234");
+    atm.selectAccount("account_3");
+    atm.deposit(150);
+    atm.withdraw(900);
+  } catch (std::runtime_error& e) { std::cout << e.what() << std::endl; } // pass
 }
 
 int main() {
   testWithDrawInsufficientFunds();
+  testWrongPinNumber();
+  testThereIsNoAccount();
+  testMinusDollar();
+  testCashInsufficientFunds();
 
   std::cout << " === Bank System Test Finished === " << std::endl;
   return 0;
